a51c4a6220aaebf2abb7fc2b707cb828fad14ee4
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-21 00:16:26 -0300

    retorna maior valor, menor, media e desvio padrao das avaliacoes

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -8,13 +8,22 @@ db.movies.aggregate(
       },
     },
     {
-      $addFields:
+      $let:
       {
-        num_favs:
-        {
-          $size: "$cast",
-        },
+        favorites:
+        [
+          "Sandra Bullock",
+          "Tom Hanks",
+          "Julia Roberts",
+          "Kevin Spacey",
+          "George Clooney",
+        ],
       },
     },
   ],
 );
+
+// criar uma variável com os atores favoritos
+// comparar cast com a nova variável com o setIntersection
+// verificar quantos tem dentro de setIntersection com o $size
+// retornar o 25 filme, apenas o titulo.
